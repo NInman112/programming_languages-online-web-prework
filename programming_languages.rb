087279bc17fa57e2bc00bfeb816e1d977f154baf
Nick Inman <nick@theoatmeal.com>
2018-11-24 23:09:24 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,9 @@ require "pry"
 def reformat_languages(languages)
   new_hash = {}
 
-  languages.each do |value, language|
-    language.each do |one, two|
+  languages.each do |value, data|
+    data.each do |language, type|
+      
          binding.pry 
     end
 
